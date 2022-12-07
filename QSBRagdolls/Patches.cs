0993b4c302458d0ca2c150b09c28aee6c6a0cfbc
Jacob <jacob@goldenneedham.com>
2022-12-07 00:20:58 -0800

    Added Player Skeleton Spawning

--- QSBRagdolls/Patches.cs
+++ QSBRagdolls/Patches.cs
@@ -1,14 +1,17 @@
 using HarmonyLib;
 using QSB.Player;
 using QSB.RespawnSync;
-namespace QSBRagdolls;
 
-[HarmonyPatch]
-public class Patches{
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(RespawnManager), nameof(RespawnManager.OnPlayerDeath))]
-    public static void AfterPlayerDeath(PlayerInfo player){
-        //We need to create a ragdoll of the player that died here
-        //To create the ragdoll we need 
+namespace QSBRagdolls{
+
+    [HarmonyPatch]
+    public class Patches{
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(RespawnManager), nameof(RespawnManager.OnPlayerDeath))]
+        public static void AfterPlayerDeath(PlayerInfo player){
+            //We need to create a ragdoll of the player that died here
+            //To create the ragdoll we need 
+            QSBRagdolls.CreateRagdoll(player);
+        }
     }
 }
--- QSBRagdolls/QSBRagdolls.cs
+++ QSBRagdolls/QSBRagdolls.cs
@@ -1,7 +1,12 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using HarmonyLib;
 using OWML.Common;
 using OWML.ModHelper;
+using QSB.Animation.Player;
+using QSB.Player;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 namespace QSBRagdolls
@@ -13,6 +18,49 @@ namespace QSBRagdolls
             instance = this;
             ModHelper.Console.WriteLine($"{nameof(QSBRagdolls)} is loaded!", MessageType.Success);
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            
+        }
+
+        private void Update(){
+            if (GetKeyDown(Key.P)){
+                CreateRagdoll(QSBPlayerManager.LocalPlayer);
+            }
+        }
+
+        public static void CreateRagdoll(PlayerInfo info){
+            if (info.Body == null)
+                return;
+
+            string isRemote = info.IsLocalPlayer ? "" : "REMOTE_";
+
+
+            var body = info.Body.transform.Find(isRemote + "Traveller_HEA_Player_v2");
+            var root = GameObject.Instantiate(body.gameObject, body.transform.position, body.transform.rotation);
+
+            //root.transform.parent = info.TransformSync.SectorDetector.GetClosestSector().Transform;
+
+            Destroy(root.GetComponent<PlayerAnimController>());
+            Destroy(root.GetComponent<Animator>());
+            Destroy(root.GetComponent<AnimatorMirror>());
+
+            root.AddComponent<CapsuleCollider>();
+            root.AddComponent<OWCapsuleCollider>();
+            var owRigidbody = root.AddComponent<OWRigidbody>();
+            root.AddComponent<ImpactSensor>();
+
+            var ss = root.AddComponent<SphereShape>();
+            
+            var dfd = root.AddComponent<DynamicForceDetector>();
+            var afd = root.AddComponent<AlignmentForceDetector>();
+            var fa = root.AddComponent<ForceApplier>();
+
+            owRigidbody.OnValidate();
+
+            owRigidbody._simulateInSector = info.TransformSync.SectorDetector.GetClosestSector().AttachedObject;
+        }
+
+        private bool GetKeyDown(Key keyCode) {
+            return Keyboard.current[keyCode].wasPressedThisFrame;
         }
 
     }
