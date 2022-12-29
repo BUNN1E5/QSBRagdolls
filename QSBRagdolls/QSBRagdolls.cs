b99ea81d22eed7bda802933bc2fe35d25e07b6ce
BUNN1E5 <BUNN1E5@users.noreply.github.com>
2022-12-28 23:07:28 -0800

    Working on the Skeleton

--- QSBRagdolls/QSBRagdolls.cs
+++ QSBRagdolls/QSBRagdolls.cs
@@ -5,6 +5,7 @@ using OWML.Common;
 using OWML.ModHelper;
 using QSB.Animation.Player;
 using QSB.Player;
+using QSB.WorldSync;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -18,11 +19,14 @@ namespace QSBRagdolls
             instance = this;
             ModHelper.Console.WriteLine($"{nameof(QSBRagdolls)} is loaded!", MessageType.Success);
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
-            
         }
 
         private void Update(){
             if (GetKeyDown(Key.P)){
+                CreateRagdoll(QSBPlayerManager.LocalPlayer);
+            }
+
+            if (GetKeyDown(Key.L)){
                 CreateRagdoll(QSBPlayerManager.LocalPlayer);
             }
         }
@@ -33,34 +37,119 @@ namespace QSBRagdolls
 
             string isRemote = info.IsLocalPlayer ? "" : "REMOTE_";
 
-
-            var body = info.Body.transform.Find(isRemote + "Traveller_HEA_Player_v2");
-            var root = GameObject.Instantiate(body.gameObject, body.transform.position, body.transform.rotation);
-
-            //root.transform.parent = info.TransformSync.SectorDetector.GetClosestSector().Transform;
-
-            Destroy(root.GetComponent<PlayerAnimController>());
-            Destroy(root.GetComponent<Animator>());
-            Destroy(root.GetComponent<AnimatorMirror>());
-
-            root.AddComponent<CapsuleCollider>();
-            root.AddComponent<OWCapsuleCollider>();
-            var owRigidbody = root.AddComponent<OWRigidbody>();
-            root.AddComponent<ImpactSensor>();
-
-            var ss = root.AddComponent<SphereShape>();
+            var playerColl = info.Body.GetComponent<Collider>();
             
-            var dfd = root.AddComponent<DynamicForceDetector>();
-            var afd = root.AddComponent<AlignmentForceDetector>();
-            var fa = root.AddComponent<ForceApplier>();
+            var body = info.Body.transform.Find(isRemote + "Traveller_HEA_Player_v2");
+            var body_copy = GameObject.Instantiate(body.gameObject, body.transform.position, body.transform.rotation);
+            
+            var p_fld = info.Body.GetComponentInChildren<DynamicFluidDetector>();
+            
+            // root.layer = LayerMask.NameToLayer("PhysicalDetector");
+            // root.tag = "DynamicPropDetector";
+            
+            Destroy(body_copy.GetComponent<PlayerAnimController>());
+            Destroy(body_copy.GetComponent<Animator>());
+            Destroy(body_copy.GetComponent<AnimatorMirror>());
+
+            // var capsuleCollider = root.AddComponent<CapsuleCollider>();
+            // capsuleCollider.height = 17f;
+            // capsuleCollider.radius = 2f;
+            // capsuleCollider.center = new Vector3(0, 8f, 0);
+
+            var owRigidbody = body_copy.AddComponent<OWRigidbody>();
+            //
+            //root.AddComponent<ImpactSensor>();
+            //
+            // var dfd = root.AddComponent<DynamicForceDetector>();
+            // var fld = root.AddComponent<DynamicFluidDetector>();
+            //
+            //
+            // fld._dragFactor = p_fld._dragFactor;
+            // fld._buoyancy = p_fld._buoyancy;
+            // fld._angularDragFactor = p_fld._angularDragFactor;
+            
 
             owRigidbody.OnValidate();
+            
+            owRigidbody.SetVelocity(info.Body.GetAttachedOWRigidbody().GetPointVelocity(info.Body.transform.position));
+            owRigidbody._simulateInSector = (Sector)((IWorldObject)info.TransformSync.SectorDetector.GetClosestSector()).AttachedObject;
 
-            owRigidbody._simulateInSector = info.TransformSync.SectorDetector.GetClosestSector().AttachedObject;
+            var parentObject = body_copy.transform.Find("Traveller_Rig_v01:Traveller_Trajectory_Jnt");
+
+            Transform[] children = body_copy.GetComponentsInChildren<Transform>();
+            
+            var mass = Locator.GetPlayerBody().GetMass() / children.Length;
+
+            foreach (Transform child in children)
+            {
+                //TODO :: GENERATE A BRAND NEW SKELETON
+                //And just set the positions of the joints to the skeleton joints
+                
+                
+                // Don't add a rigidbody or CharacterJoint to the parent object
+                if (child == parentObject.transform)
+                    continue;
+
+                if (!child.transform.name.Contains("Traveller_Rig_v01")) //Oof a string compare
+                    continue;
+
+                var parent = child.transform.parent;
+
+                // Add a rigidbody to the child object
+                Rigidbody rb = child.gameObject.AddComponent<Rigidbody>();
+                rb.useGravity = false;
+                rb.mass = mass;
+                
+                // Add a CharacterJoint to the child object
+                //CharacterJoint cj = child.gameObject.AddComponent<CharacterJoint>();
+                HingeJoint cj = child.gameObject.AddComponent<HingeJoint>();
+                cj.connectedBody = parent.GetComponent<Rigidbody>();
+                cj.enableCollision = false;
+
+                var cc = child.gameObject.AddComponent<CapsuleCollider>();
+                cc.height = 10 * Vector3.Distance(cc.transform.position, parent.position) / 2;
+                cc.radius = Mathf.Sqrt(cc.height);
+                
+                Physics.IgnoreCollision(playerColl, cc);
+                Physics.IgnoreCollision(playerColl, cc);
+                
+                var owr = child.gameObject.AddComponent<OWRigidbody>();
+                owr.OnValidate();
+
+                child.GetComponent<CenterOfTheUniverseOffsetApplier>().enabled = false;
+
+                var ic = child.gameObject.AddComponent<IgnoreCollision>();
+                ic._ignorePlayer = true;
+
+                var c_dfd = cc.gameObject.AddComponent<DynamicForceDetector>();
+                var c_fld = cc.gameObject.AddComponent<DynamicFluidDetector>();
+
+                c_fld._dragFactor = p_fld._dragFactor;
+                c_fld._buoyancy = p_fld._buoyancy;
+                c_fld._angularDragFactor = p_fld._angularDragFactor;
+
+                owr.transform.parent = parent;
+                
+                owr.SetVelocity(info.Body.GetAttachedOWRigidbody().GetVelocity());
+                owr.SetAngularVelocity(info.Body.GetAttachedOWRigidbody().GetAngularVelocity());
+                owr._simulateInSector = (Sector)((IWorldObject)info.TransformSync.SectorDetector.GetClosestSector()).AttachedObject;
+
+                child.gameObject.layer = LayerMask.NameToLayer("PhysicalDetector");
+                child.tag = "DynamicPropDetector";
+                
+                // Set the anchor and connected anchor of the CharacterJoint
+                // to the center of the child object
+            }
+
+            //parentObject.GetComponent<CenterOfTheUniverseOffsetApplier>().enabled = true;
         }
 
         private bool GetKeyDown(Key keyCode) {
             return Keyboard.current[keyCode].wasPressedThisFrame;
+        }
+        
+        private bool GetKey(Key keyCode) {
+            return Keyboard.current[keyCode].isPressed;
         }
 
     }
